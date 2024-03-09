926f7018823099f635684c71b66a82484c4cd5e4
justyna38 <warzecha.justyna93@gmail.com>
2024-03-09 14:38:18 +0100

    test

---
+++ Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Justyna Warzecha on 09/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
